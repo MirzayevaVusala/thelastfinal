b58ff36e65912e622d1535c89cf37023fd09a7a3
Vusala <vmirzeyeva197@gmail.com>
2024-12-27 09:02:27 +0400

    second deploy

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<HashRouter >
-    <App />
-    </HashRouter>
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+} else {
+  console.error("Root element not found!");
+}
